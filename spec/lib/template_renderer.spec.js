7e427e8130f24349af0667c4425fe9d0298c5dde
ycombinator <ycombinator@gmail.com>
2013-04-15 16:56:07 -0700

    [#4] Adding another test for template renderer.

--- spec/lib/template_renderer.spec.js
+++ spec/lib/template_renderer.spec.js
@@ -2,15 +2,31 @@ var templateRenderer = require('../../lib/template_renderer.js')
 
 describe("Template Renderer", function() {
 
+  var renderer
+
+  beforeEach(function() {
+    renderer = templateRenderer({
+      templatesDir: __dirname + '/../fixtures/templates/'
+    })
+  }) // END - beforeEach
+    
   it ("should fail if non existent template name is used", function(done) {
-
-    var renderer = templateRenderer('../fixtures/templates')
-
+    
     renderer.render("non-existent-template", {}, function(err, text) {
       expect(err).not.toBe(null)
       done()
     })
+    
+  }) // END - it should fail if non existent template name is used
+  
+  it ("should succeed if existing template name is used", function(done) {
 
-  })
+    renderer.render("simple-template", { caseNumber: '13-T-000111' }, function(err, text) {
+      expect(err).toBe(null)
+      expect(text).toBe("This should be a case number: 13-T-000111.")
+      done()
+    })
+
+  }) // END - it should fail if non existent template name is used
 
 })
