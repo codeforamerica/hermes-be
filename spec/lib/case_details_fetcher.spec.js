f1a111548f9a2df5e491deb94ca63a4105e3cacb
ycombinator <ycombinator@gmail.com>
2013-04-16 06:38:33 -0700

    [#9] Initial code to lookup case in CourtNet, given case #.

--- spec/lib/case_details_fetcher.spec.js
+++ spec/lib/case_details_fetcher.spec.js
@@ -1,10 +1,13 @@
-var fetcher = require('../../lib/case_details_fetcher.js')
+var caseDetailsFetcher = require('../../lib/case_details_fetcher.js')
 
 describe("Case Details Fetcher", function() {
 
   it ("should fetch the search form page", function(done) {
     
-    fetcher("13-T-000001", function(err, details) {
+    var fetcher = caseDetailsFetcher("13-T-000001")
+    fetcher.fetch(function(err, details) {
+      expect(details.title).toBe('COMMONWEALTH VS. CATTAN, JOSEPH RAY')
+      expect(details.nextCourtLocation).toBe('HJ302')
       done()
     })
     
