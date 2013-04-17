26de16844ad73b797bf9eb74d58731f575fcd263
ycombinator <ycombinator@gmail.com>
2013-04-17 16:01:29 -0700

    [#15] Parse negation messages.

--- spec/lib/message_parser.spec.js
+++ spec/lib/message_parser.spec.js
@@ -140,6 +140,142 @@ describe("Message Parser", function() {
       
     }) // END it - should NOT accept yeppy
   
-  })
+  }) // END describe - for affirmations
+
+  describe("for negations", function() {
+
+    it ("should accept n", function(done) {
+
+      var parser = messageParser("n")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept n
+  
+    it ("should accept N", function(done) {
+
+      var parser = messageParser("N")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept N
+
+    it ("should accept no", function(done) {
+
+      var parser = messageParser("no")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept no
+  
+    it ("should accept NO", function(done) {
+
+      var parser = messageParser("NO")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept NO
+  
+    it ("should accept No", function(done) {
+
+      var parser = messageParser("No")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept No
+  
+    it ("should accept nah", function(done) {
+
+      var parser = messageParser("nah")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept nah
+  
+    it ("should accept nope", function(done) {
+
+      var parser = messageParser("nope")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept nope
+  
+    it ("should accept never", function(done) {
+
+      var parser = messageParser("never")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept never
+  
+    it ("should accept nada", function(done) {
+
+      var parser = messageParser("nada")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept nada
+  
+    it ("should accept nopes", function(done) {
+
+      var parser = messageParser("nopes")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept nopes
+  
+    it ("should accept nein", function(done) {
+
+      var parser = messageParser("nein")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_NEGATION)
+        done()
+      })
+      
+    }) // END it - should accept nein
+  
+    it ("should NOT accept negative", function(done) {
+
+      var parser = messageParser("negative")
+      parser.parse(function(err, result) {
+        expect(err).toBe(null)
+        expect(result.type).toBe(parser.MESSAGE_TYPE_OTHER)
+        done()
+      })
+      
+    }) // END it - should NOT accept negative
+  
+  }) // END describe - for negations
 
 })
