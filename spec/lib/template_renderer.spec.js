9c44a50695f848433327668d8f437168338dae2b
ycombinator <ycombinator@gmail.com>
2013-04-17 18:44:43 -0700

    Adding method to check if template exists.

--- spec/lib/template_renderer.spec.js
+++ spec/lib/template_renderer.spec.js
@@ -10,23 +10,49 @@ describe("Template Renderer", function() {
     })
   }) // END - beforeEach
     
-  it ("should fail if non existent template name is used", function(done) {
+  describe ("for render", function() {
+
+    it ("should fail if non existent template name is used", function(done) {
+      
+      renderer.render("non-existent-template", {}, function(err, text) {
+        expect(err).not.toBe(null)
+        done()
+      })
+      
+    }) // END - it should fail if non existent template name is used
     
-    renderer.render("non-existent-template", {}, function(err, text) {
-      expect(err).not.toBe(null)
-      done()
+    it ("should succeed if existing template name is used", function(done) {
+      
+      renderer.render("simple-template", { caseNumber: '13-T-000111' }, function(err, text) {
+        expect(err).toBe(null)
+        expect(text).toBe("This should be a case number: 13-T-000111.")
+        done()
+      })
+      
+    }) // END - it should fail if non existent template name is used
+    
+  }) // END describe - for render
+
+  describe ("for exists", function() {
+
+    it ("should return true if template exists", function(done) {
+
+      renderer.exists("simple-template", function(exists) {
+        expect(exists).toBe(true)
+        done()
+      })
+
+    })
+
+    it ("should return false if template does NOT exist", function(done) {
+
+      renderer.exists("non-existent-template", function(exists) {
+        expect(exists).toBe(false)
+        done()
+      })
+      
     })
     
-  }) // END - it should fail if non existent template name is used
-  
-  it ("should succeed if existing template name is used", function(done) {
-
-    renderer.render("simple-template", { caseNumber: '13-T-000111' }, function(err, text) {
-      expect(err).toBe(null)
-      expect(text).toBe("This should be a case number: 13-T-000111.")
-      done()
-    })
-
-  }) // END - it should fail if non existent template name is used
+  }) // END describe - for exists
 
 })
