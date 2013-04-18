ee120d71f944e03a1fc64cfe43af338ab45e7c07
ycombinator <ycombinator@gmail.com>
2013-04-18 11:33:36 -0700

    Adding more tests for cell # normalization.

--- spec/models/contact.spec.js
+++ spec/models/contact.spec.js
@@ -32,6 +32,18 @@ describe("Contact model", function() {
 
   describe("for normalizeCellNumber", function() {
 
+    it ("should return number without dashes if phone number with dashes is given", function() {
+      expect(models.contact.normalizeCellNumber('123-4569')).toBe('1234569')
+    })
+
+    it ("should return number with leading +1 if 10-digit phone number is given", function() {
+      expect(models.contact.normalizeCellNumber('405-123-4569')).toBe('+14051234569')
+    })
+
+    it ("should return number with leading + if 11-digit phone number with leading 1 is given", function() {
+      expect(models.contact.normalizeCellNumber('1 (405) 123-4569')).toBe('+14051234569')
+    })
+
   }) // END describe - for normalizeCellNumber
 
 }) // END describe - for Contact model
