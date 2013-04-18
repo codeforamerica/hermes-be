7679cb3efdd1f95a95f5f049b0d59f266c532440
ycombinator <ycombinator@gmail.com>
2013-04-18 09:41:56 -0700

    Refactoring case # regexp into case model.

--- models/case.js
+++ models/case.js
@@ -3,11 +3,23 @@ var sequelize = require('../lib/sequelize.js'),
     Contact = require('./contact.js'),
     Message = require('./message.js')
 
+var CASE_NUMBER_REGEXP = /(\d[1-9]?)-?([A-Za-z]+)-?(\d{0,5}[1-9])-?(\d{0,2}[1-9])?/
+
 var Case = sequelize.define('cases', {
   number: Sequelize.STRING,
   title: Sequelize.STRING,
   next_court_datetime: Sequelize.DATE,
   next_court_location: Sequelize.STRING
+}, {
+  classMethods: {
+    getCaseNumberRegexp: function() {
+      return CASE_NUMBER_REGEXP
+    },
+
+    isValidNumber: function(number) {
+      return (number.match(CASE_NUMBER_REGEXP) != null)
+    }
+  }
 })
 
 Case.hasMany(Contact, { joinTableName: 'case_contacts', foreignKey: 'case_id' })
