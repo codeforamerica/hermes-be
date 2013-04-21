6801e907e6e05b87a1ea00e0c8a12dc70336acc5
ycombinator <ycombinator@gmail.com>
2013-04-21 13:00:28 -0700

    Unit test for origin server not being reachable for case lookup.

---
+++ spec/fakes/case_details_fetcher.js
@@ -0,0 +1,70 @@
+module.exports = function() {
+
+  var details = {
+    title: null,
+    nextCourtDateTime: null,
+    nextCourtLocation: null,
+    defendantFirstName: null,
+    defendantMiddleName: null,
+    defendantLastName: null
+  }
+  
+  var error
+  
+  return {
+
+    setTitle: function(title) {
+      details.title = title
+      return this
+    },
+
+    setNextCourtDateTime: function(nextCourtDateTime) {
+      details.nextCourtDateTime = nextCourtDateTime
+      return this
+    },
+
+    setNextCourtLocation: function(nextCourtLocation) {
+      details.nextCourtLocation = nextCourtLocation
+      return this
+    },
+
+    setDefendantFirstName: function(defendantFirstName) {
+      details.defendantFirstName = defendantFirstName
+      return this
+    },
+
+    setDefendantMiddleName: function(defendantMiddleName) {
+      details.defendantMiddleName = defendantMiddleName
+      return this
+    },
+
+    setDefendantLastName: function(defendantLastName) {
+      details.defendantLastName = defendantLastName
+      return this
+    },
+
+    setError: function(err) {
+      error = err
+      return this
+    },
+
+    build: function() {
+      return function(caseNumber) {
+
+        return {
+          fetch: function(cb) {
+            if (error) {
+              cb(error)
+            } else {
+              cb(null, details)
+            }
+          }
+        }
+
+      }
+
+    }
+
+  }
+  
+}
--- spec/lib/message_processor.spec.js
+++ spec/lib/message_processor.spec.js
@@ -1,7 +1,8 @@
 /* jshint -W117 */ // For undeclared symbols like 'describe', 'it' and 'expect'    
 var config = require('config'),
     messageProcessor = require('../../lib/message_processor.js'),
-    sequelize = require('../../lib/sequelize.js')
+    sequelize = require('../../lib/sequelize.js'),
+    fakeCaseDetailsFetcher = require('../fakes/case_details_fetcher.js')
 
 describe('Message Processor', function() {
 
@@ -70,6 +71,35 @@ describe('Message Processor', function() {
         }) // END it - should save message
 
       }) // END describe - and message is unrecognized
+
+      describe('and message is case number', function() {
+
+        describe('and origin server is unreachable', function() {
+
+          var fetcher
+          
+          beforeEach(function() {
+            fetcher = fakeCaseDetailsFetcher()
+              .setError('ECONNREFUSED')
+              .build()
+          })
+
+          it ('should respond with cannot find case response', function(done) {
+            
+            var expectedResponse = 'We’re sorry, but we can’t send you a reminder about this case. Please make sure the case number is correct, or call (999) 999-9999.'
+           
+            var processor = messageProcessor('+1999999FROM', '+122222222TO', '13-T-000001', null, null, { caseDetailsFetcher: fetcher })
+            
+            processor.process(function(err, actualResponse) {
+              expect(actualResponse).toBe(expectedResponse)
+              done()
+            })
+            
+          }) // END it - should respond with unrecognized message response
+          
+        }) // END describe - and origin server is unreachable
+
+      }) // END describe - and message is case number
 
     }) // END describe - where contact has no subscriptions
 
