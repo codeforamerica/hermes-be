b3fc982c7fb50e846c90c831571a07c8000cc88c
ycombinator <ycombinator@gmail.com>
2013-04-21 11:30:52 -0700

    Adding more message processor tests.

--- spec/lib/message_processor.spec.js
+++ spec/lib/message_processor.spec.js
@@ -18,12 +18,23 @@ describe('Message Processor', function() {
         it ('should respond with unrecognized message response', function(done) {
 
           var expectedResponse = 'Sorry, I didn\'t understand that. Please call ' + config.responses.clerkPhone + ' with questions.'
+
+          var processor = messageProcessor('+1999999FROM', '+122222222TO', 'abracadabra', null, null)
+
+          processor.process(function(err, actualResponse) {
+            expect(actualResponse).toBe(expectedResponse)
+            done()
+          })
+
+        }) // END it - should respond with unrecognized message response
+
+        it ('should save contact', function(done) {
+
           var expectedFromCellNumber = '+1999999FROM'
 
           var processor = messageProcessor(expectedFromCellNumber, '+122222222TO', 'abracadabra', null, null)
 
           processor.process(function(err, actualResponse) {
-            expect(actualResponse).toBe(expectedResponse)
             sequelize.query('SELECT * FROM contacts')
               .success(function(results) {
                 expect(results.length).toBe(1)
@@ -33,7 +44,29 @@ describe('Message Processor', function() {
               .error(done)
           })
 
-        }) // END it - should respond with unrecognized message response
+        }) // END it - should save contact
+
+        it ('should save message', function(done) {
+
+          var expectedFromCellNumber = '+1999999FROM',
+              expectedToNumber = '+122222222TO',
+              expectedMessage = 'abracadabra'
+
+          var processor = messageProcessor(expectedFromCellNumber, expectedToNumber, expectedMessage, null, null)
+
+          processor.process(function(err, actualResponse) {
+            sequelize.query('SELECT * FROM messages')
+              .success(function(results) {
+                expect(results.length).toBe(1)
+                expect(results[0].sender).toBe(expectedFromCellNumber)
+                expect(results[0].recipient).toBe(expectedToNumber)
+                expect(results[0].body).toBe(expectedMessage)
+                done()
+              })
+              .error(done)
+          })
+
+        }) // END it - should save message
 
       }) // END describe - and message is unrecognized
 
