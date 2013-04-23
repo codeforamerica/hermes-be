9f705a90e260cf70f33f31d9ba32f80cddac6e86
ycombinator <ycombinator@gmail.com>
2013-04-23 09:34:25 -0700

    [#21] Adding event types.

--- models/event.js
+++ models/event.js
@@ -6,6 +6,19 @@ var sequelize = require('../lib/sequelize.js'),
 var Event = sequelize.define('events', {
   type: { type: Sequelize.STRING, allowNull: false },
   data: { type: Sequelize.TEXT }
+}, {
+  classMethods: {
+    types: function() {
+      return {
+        MANUAL_INPUT: 'MANUAL_INPUT',
+        CASE_DETAILS_RETRIVED_FROM_ORIGIN_SERVER: 'CASE_DETAILS_RETRIEVAL_FROM_ORIGIN_SERVER',
+        CASE_DETAILS_UPDATED_FROM_ORIGIN_SERVER: 'CASE_DETAILS_UPDATE_FROM_ORIGIN_SERVER',
+        SMS_INBOUND: 'SMS_INBOUND',
+        SMS_OUTBOUND: 'SMS_OUTBOUND'
+
+      }
+    }
+  }
 })
 
 Case.hasMany(Event)
